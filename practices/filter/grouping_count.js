d7a24d36e3f881f51d74e64b9c940d6a52fbffb9
FelicityChen <felicity.chen@oocl.com>
2019-07-10 14:55:34 +0800

    business logic: finish filter and map's practice

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,6 +3,12 @@
 function grouping_count(collection) {
 
   //implement here
+  let result = {};
+  collection.reduce((obj, item) => {
+    obj[item]? obj[item]++: obj[item] = 1;
+    return obj;
+  }, result);
+  return result;
 }
 
 module.exports = grouping_count;
